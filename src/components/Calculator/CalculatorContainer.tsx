ae505f89ff19eb2389f785331b6b1cc274240dd8
dyandev <diego6mm@gmail.com>
2024-11-26 00:45:14 -0400

    fix: change border opacity of 'CalculatorContainer'

--- src/components/Calculator/CalculatorContainer.tsx
+++ src/components/Calculator/CalculatorContainer.tsx
@@ -4,8 +4,8 @@ export const CalculatorContainer = ({ children }: PropsWithChildren) => {
   return (
     <article
       className="flex flex-col gap-8 min-h-[300px] min-w-[300px] 
-      max-w-[540px] m-auto bg-stone-950/10 sm:bg-green-950/10 sm:backdrop-blur-sm sm:backdrop sm:backdrop-brightness-200 
-      p-4 rounded-3xl border border-transparent sm:border-green-950"
+      max-w-[540px] m-auto bg-stone-950/10 sm:bg-green-950/10 sm:backdrop-blur-sm sm:backdrop-brightness-200 
+      p-4 rounded-3xl border border-transparent sm:border-green-800/40"
     >
       {children}
     </article>
