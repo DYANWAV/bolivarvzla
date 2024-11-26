9fb3311958ff8aeda04eda91831b21ec8d5eddf2
dyandev <diego6mm@gmail.com>
2024-11-26 00:56:59 -0400

    fix: changing CalculatorContainer background color and adding box-shadow

--- src/components/Calculator/CalculatorContainer.tsx
+++ src/components/Calculator/CalculatorContainer.tsx
@@ -3,11 +3,12 @@ import { PropsWithChildren } from 'react'
 export const CalculatorContainer = ({ children }: PropsWithChildren) => {
   return (
     <article
-      className="flex flex-col gap-8 min-h-[300px] min-w-[300px] 
-      max-w-[540px] m-auto bg-stone-950/10 sm:bg-green-950/10 sm:backdrop-blur-sm sm:backdrop-brightness-200 
-      p-4 rounded-3xl border border-transparent sm:border-green-800/40"
+      className="flex flex-col gap-8 min-h-[300px] min-w-[300px] sm:shadow-lg sm:shadow-black/60
+      max-w-[540px] m-auto p-4 rounded-3xl border border-transparent sm:border-green-800/30"
     >
       {children}
     </article>
   )
 }
+
+// bg-stone-950/10 sm:bg-green-950 sm:backdrop-blur-sm sm:backdrop-brightness-200
