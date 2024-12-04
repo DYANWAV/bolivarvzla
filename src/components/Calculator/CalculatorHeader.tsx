9797cd414d2d6f3df108db8c676732df540a3807
dyandev <diego6mm@gmail.com>
2024-12-04 07:11:21 -0400

    Fix: Cambio de título en en 'CalculatorHeader' y agregando atributo 'translate="no"

--- src/components/Calculator/CalculatorHeader.tsx
+++ src/components/Calculator/CalculatorHeader.tsx
@@ -2,8 +2,10 @@ import { ResetButton } from '@components'
 
 export const CalculatorHeader = () => {
   return (
-    <div className='text-left flex justify-between items-center'>
-      <h2 className='text-xl sm:text-2xl font-bold uppercase'>DolarVzla</h2>
+    <div className="text-left flex justify-between items-center">
+      <h2 className="text-xl sm:text-2xl font-bold uppercase" translate="no">
+        Bolivarvzla
+      </h2>
       <ResetButton />
     </div>
   )
